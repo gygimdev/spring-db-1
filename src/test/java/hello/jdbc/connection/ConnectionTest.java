4f898d5b71ff124f15dcbbc2333c4a69e228c2ee
gygim <gygim.dev@gmail.com>
2023-04-27 18:07:49 +0900

    커낵션풀과 데이터 소스 이해

---
+++ src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -0,0 +1,55 @@
+package hello.jdbc.connection;
+
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+
+@Slf4j
+public class ConnectionTest {
+    // 설정과 사용의 분리!!
+    @Test
+    void driverManager() throws SQLException {
+        // DriverManager 는 커넥션을획득할 때마다 URL, USERNAME, PASSWORD를 계속 전달해야한다.
+        Connection con1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        Connection con2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+        log.info("connection={}, class={}", con1, con1.getClass());
+        log.info("connection={}, class={}", con2, con2.getClass());
+    }
+    
+    @Test
+    void dataSourceDriverManager() throws SQLException {
+        // DataSource 방식은 처음 객체를 생성할 때만 필요한 파라미터를 넘겨주고, 커낵션을 획득할 때는 단순히 getConnection을 호출하면된다.
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        useDataSource(dataSource);
+    }
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        //커넥션 풀링:
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
+        useDataSource(dataSource);
+        Thread.sleep(1000);
+    }
+
+    private void useDataSource(DataSource dataSource) throws SQLException {
+        Connection con1 = dataSource.getConnection();
+        Connection con2 = dataSource.getConnection();
+        log.info("connection={}, class={}", con1, con1.getClass());
+        log.info("connection={}, class={}", con2, con2.getClass());
+    }
+}
